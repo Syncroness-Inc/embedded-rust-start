513f5869b21a4e59e40a5ca9af18a7fc3a2eb3dd
seanybaggins <seli2854@colorado.edu>
2020-08-12 22:00:50 +0000

    trying to get rid of false negatives in build

--- scripts/check/all.py
+++ scripts/check/all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+import sys
 
 check_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -7,8 +8,8 @@ check_scripts = [check_script for check_script in os.listdir(check_directory) if
 
 # Run all checks and ruturn the last known error code.
 # Otherwise exit with status code 0.
-status_code = None
+status_code = 0
 for check_script in check_scripts:
     status_code = os.system(check_directory + "/" + check_script)
 
-exit(status_code)
+sys.exit(status_code)
