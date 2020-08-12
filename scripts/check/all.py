78e8195822441851fd1e8103dc69cf9b999efca2
seanybaggins <seli2854@colorado.edu>
2020-08-12 22:05:49 +0000

    trying to get rid of flase nagative on build

--- scripts/check/all.py
+++ scripts/check/all.py
@@ -8,8 +8,10 @@ check_scripts = [check_script for check_script in os.listdir(check_directory) if
 
 # Run all checks and ruturn the last known error code.
 # Otherwise exit with status code 0.
-status_code = 0
+exit_status_code = 0
 for check_script in check_scripts:
-    status_code = os.system(check_directory + "/" + check_script)
+    tmp_status_code = os.system(check_directory + "/" + check_script)
+    if tmp_status_code != 0:
+        exit_status_code = tmp_status_code
 
-sys.exit(status_code)
+sys.exit(exit_status_code)
